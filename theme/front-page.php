6005a0a59d518d16b1a300d6eb7cf50e33b28a73
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-12-03 03:16:50 +0700

    add google fonts - Noto Sans Thai

--- theme/front-page.php
+++ theme/front-page.php
@@ -5,11 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<!-- Google Fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap" rel="stylesheet">
-
 	<?php wp_head(); ?>
 </head>
 
